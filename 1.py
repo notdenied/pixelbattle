e878e90c76411a68ec041a86995f7da26f8aec25
Andrey Ryzhov <denied@c3po.ru>
2024-07-27 17:19:18 +0300

    Commit from Linux.

---
+++ 1.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.websockets import WebSocket
+
+
+app = FastAPI()
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"You said: {data}")
